b5a1aed1a2e80079a749d15bc92cf4a8517159f0
Manuel Bugallo <marvinody@gmail.com>
2019-10-01 19:17:52 -0400

    feat: adds post route for create user

--- src/routes/api/users/index.js
+++ src/routes/api/users/index.js
@@ -4,3 +4,13 @@ export async function get(req, res) {
   const users = await User.findAll();
   res.json(users)
 }
+
+export async function post(req, res) {
+  try {
+    const user = await User.create(req.body)
+    res.json(user)
+  } catch (err) {
+    console.error(err)
+    res.sendStatus(500)
+  }
+}
