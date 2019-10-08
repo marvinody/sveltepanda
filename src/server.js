359590cf615115fb0a054f7aeb572137fbe47edd
Manuel Bugallo <marvinody@gmail.com>
2019-10-07 22:09:08 -0400

    feat: installs some session and middleware stuff

--- src/server.js
+++ src/server.js
@@ -1,9 +1,15 @@
 import * as sapper from '@sapper/server';
 import bodyParser from 'body-parser';
 import compression from 'compression';
+import ConnectSequelizeStore from 'connect-session-sequelize';
 import express from 'express';
+import session from 'express-session';
+import morgan from 'morgan';
 import sirv from 'sirv';
 import db from './db';
+
+const sequelizeStore = ConnectSequelizeStore(session.Store)
+const sessionStore = new sequelizeStore({ db })
 
 const { PORT, NODE_ENV, SEED } = process.env;
 const dev = NODE_ENV === 'development';
@@ -14,8 +20,15 @@ if (dev) {
 
 express() // You can also use Express
 	.use(
+		morgan('dev'),
 		bodyParser.json(),
 		compression({ threshold: 0 }),
+		session({
+			secret: process.env.SESSION_SECRET || 'VERY NICE. I LIKE YOU. HI-5',
+			store: sessionStore,
+			resave: false,
+			saveUninitialized: false
+		}),
 		sirv('static', { dev }),
 		sapper.middleware(),
 
