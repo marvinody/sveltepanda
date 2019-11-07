e781724bd0d3f5b22b204effa9e90cda19fe5b81
Manuel Bugallo <marvinody@gmail.com>
2019-11-07 00:14:19 -0500

    refactor: simplifies all galleries by reducing include obj

--- src/routes/api/galleries/index.js
+++ src/routes/api/galleries/index.js
@@ -1,13 +1,10 @@
-import { File, Gallery } from '../../../db'
+import { Gallery } from '../../../db'
 export async function get(req, res, next) {
   try {
     const galleries = await Gallery.findAll({
-      // [sequelize.fn('COUNT', sequelize.col('Files.id')), 'pages']
-      include: [{
-        model: File,
-        as: 'pages',
-      },
-        'coverPage', 'uploader'],
+      include: [
+        'pages', 'coverPage', 'uploader'
+      ],
     })
     res.json(galleries)
   } catch (err) {
